e22ff43158b89f7db9ba15712e451f6da5417d08
dakersalex <alex.dakers@gmail.com>
2026-04-02 11:39:56 +0200

    feat: Briefing Generator tab with topic/period/type/guidance selectors

--- tmp_grep.py
+++ tmp_grep.py
@@ -1,5 +1,9 @@
 import subprocess
-r = subprocess.run(['grep', '-n', 'getThemeArticles', '/Users/alexdakers/meridian-server/meridian.html'], capture_output=True, text=True)
-with open('/Users/alexdakers/meridian-server/tmp_getTheme.txt', 'w') as f:
-    f.write(r.stdout)
+r1 = subprocess.run(['sed', '-n', '377,430p', '/Users/alexdakers/meridian-server/meridian.html'], capture_output=True, text=True)
+r2 = subprocess.run(['sed', '-n', '3184,3220p', '/Users/alexdakers/meridian-server/meridian.html'], capture_output=True, text=True)
+r3 = subprocess.run(['sed', '-n', '3580,3600p', '/Users/alexdakers/meridian-server/meridian.html'], capture_output=True, text=True)
+with open('/Users/alexdakers/meridian-server/tmp_scan.txt', 'w') as f:
+    f.write("=== FOLDER CSS ===\n" + r1.stdout)
+    f.write("\n=== switchMode ===\n" + r2.stdout)
+    f.write("\n=== init area ===\n" + r3.stdout)
 print("DONE")
